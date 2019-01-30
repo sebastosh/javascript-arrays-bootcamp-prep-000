b7cd8c6909d146e0d08234c5b85efcc2273b4b47
Sebastien Sanz de Santamaria <sebastosh@pm.me>
2019-01-30 02:52:58 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -34,7 +34,7 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array){
-  return array.slice((0, array.length - 1))
+  return array.slice(0, array.length - 1)
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
